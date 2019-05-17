f1afefa939e6d44073e73b686554092175f49233
zhouxiaohu <80839961@yonghui.com>
2019-05-17 14:21:55 +0800

    edge struct finish

---
+++ src/main/java/com/zxg/graph/DirectedWeighedGraph.java
@@ -0,0 +1,21 @@
+package com.zxg.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Created by zhou1 on 2019/5/17.
+ *
+ * 使用邻接表来表示带权重的有向图
+ */
+public class DirectedWeighedGraph {
+
+    HashMap<String,HashSet<DirectedWeighedEdge>> adj;
+
+    public DirectedWeighedGraph(String input){
+        String[] inputList = input.split(",");
+
+    }
+
+
+}
