811d467b46528f5cce7f898ccd251583259d10e2
zhouxiaohu <80839961@yonghui.com>
2019-05-17 14:32:22 +0800

    graph init function ready

--- src/main/java/com/zxg/graph/DirectedWeighedGraph.java
+++ src/main/java/com/zxg/graph/DirectedWeighedGraph.java
@@ -2,6 +2,7 @@ package com.zxg.graph;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Created by zhou1 on 2019/5/17.
@@ -13,9 +14,34 @@ public class DirectedWeighedGraph {
     HashMap<String,HashSet<DirectedWeighedEdge>> adj;
 
     public DirectedWeighedGraph(String input){
-        String[] inputList = input.split(",");
+        adj = new HashMap<>();
 
+        String[] inputList = input.split(",");
+        for (String edgeExpress: inputList){
+            String edgeTrimed = edgeExpress.trim();
+            try{
+                DirectedWeighedEdge edgeObjct = new DirectedWeighedEdge(edgeTrimed);
+
+                HashSet<DirectedWeighedEdge> nodeAdjacentEdges = adj.get(edgeObjct.getFrom());
+                if (nodeAdjacentEdges == null) {
+                    nodeAdjacentEdges = new HashSet<>();
+                }
+                nodeAdjacentEdges.add(edgeObjct);
+                adj.put(edgeObjct.getFrom(),nodeAdjacentEdges);
+            }catch (Exception e){
+                System.err.printf("%s parse error %s, skip!!!\n",edgeExpress,e.getMessage());
+                continue;
+            }
+        }
     }
 
+    public static void main(String[] args){
+        String input = "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7";
+        DirectedWeighedGraph graph = new DirectedWeighedGraph(input);
+        for(Map.Entry<String,HashSet<DirectedWeighedEdge>> entry: graph.adj.entrySet()){
+            System.err.println(entry.getKey());
+            System.err.println(entry.getValue());
+        }
+    }
 
 }
