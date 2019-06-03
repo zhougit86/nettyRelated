5feaaa96483e2ecf920e42cce175a8f3d86bd2d3
zhouxiaohu <80839961@yonghui.com>
2019-06-03 17:00:47 +0800

    temp save

--- src/main/java/netty/ch1/client/EchoClient.java
+++ src/main/java/netty/ch1/client/EchoClient.java
@@ -1,9 +1,14 @@
 package netty.ch1.client;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
 
 /**
  * Created by zhou1 on 2019/6/3.
@@ -21,7 +26,25 @@ public class EchoClient {
         EventLoopGroup group = new NioEventLoopGroup();
         try{
             Bootstrap b = new Bootstrap();
-            b.group(group).channel(NioSocketChannel.class).remoteAddress(new In)
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host,port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully().sync();
         }
+    }
+
+    public static void main(String[] args) throws Exception{
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        new EchoClient(host, port).start();
     }
 }
